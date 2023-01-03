b615f3c27ec73f884012adbfcbc5e55ca957f831
Abner Freitas <defreitasabner@gmail.com>
2023-01-03 17:28:44 -0300

    Starting to create the ProgressClocksList provider.

---
+++ lib/models/progress_clocks_list.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:progress_clocks_fitd_flutter/models/progress_clock.dart';
+
+class ProgressClocksList extends ChangeNotifier {
+  
+  List<ProgressClock> progressClocksList;
+
+  ProgressClocksList({
+    required this.progressClocksList
+  });
+
+  void addProgressClock({
+    required int id, 
+    required String name, 
+    required int maxProgressLevel, 
+    required int currentProgressLevel, 
+    String? description
+    }) {
+
+    ProgressClock(
+      id: id, 
+      name: name, 
+      maxProgressLevel: maxProgressLevel, 
+      currentProgressLevel: currentProgressLevel,
+      description: description != null? description : ''
+    );
+
+  }
+
+  void removeProgressClock() {
+
+  }
+
+  void editProgressClock() {
+
+  }
+
+}
